1d493b5a61cdb636055aa1f8a06c060124fc7478
Opoku-Wiafe <opokuwiafevan@gmail.com>
2024-01-16 20:14:07 -0800

    final airbnb push

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""__init__ method for model dictionary"""
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Medule that defines the Amenity class"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """representation of the Amenity class and its name"""
+
+    name = ""
